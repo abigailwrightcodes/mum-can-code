3771db2848e0af87dc0f65fe5ab7cfaf8a982dcc
abigailwright <abigailwright89@gmail.com>
2024-10-03 14:31:36 +0100

    Home page setup

--- mum-can-code/src/App.jsx
+++ mum-can-code/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './components/Home.jsx';
+import '../src/css/home.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <h1>Mum can code</h1>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/store">Store</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/community">Community</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+          
+          
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+
+          
+      
+        </Routes>
       </div>
-    </>
+    </Router>
   )
 }
 
